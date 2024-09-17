780710a83551969bbf2881d687f249a144dcb1b9
Hamin Lee <whereanna7@gmail.com>
2024-09-17 01:23:23 +0100

    [ADD] add accordion panels, floating indicator

--- src/components/ArrayPracticeContainer.jsx
+++ src/components/ArrayPracticeContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AddValue from "./questions/AddValue";
 import RemoveLastValue from "./questions/RemoveLastValue";
 import RemoveFirstValue from "./questions/RemoveFirstValue";
@@ -6,27 +6,128 @@ import InsertValueAtFirst from "./questions/InsertValueAtFirst";
 import FindValue from "./questions/FindValue";
 import SortArray from "./questions/SortArray";
 import ReverseArray from "./questions/ReverseArray";
-import FilterOutEvenValubes from "./questions/FilterOutEvenValues";
+import FilterOutEvenValues from "./questions/FilterOutEvenValues";
 import TransformValues from "./questions/TransformValues";
 import SumAllValues from "./questions/SumAllValues";
 
 function ArrayPracticeContainer() {
+  const [openAccordion, setOpenAccordion] = useState(null);
+  const [selectedButton, setSelectedButton] = useState(1);
+
+  const toggleAccordion = (id) => {
+    setOpenAccordion((prevId) => (prevId === id ? null : id));
+  };
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(`section${id}`);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const toggleIndicatorButton = (id) => {
+    setSelectedButton(id);
+  };
+
   return (
     <div className="practice-container">
+      <div
+        className={`scroll-button-container ${
+          openAccordion === 1 ? "open" : ""
+        }`}
+      >
+        {Array.from({ length: 20 }, (_, index) => (
+          <button
+            key={index}
+            className={`scroll-button ${
+              selectedButton === index + 1 ? "selected" : ""
+            }`}
+            onClick={() => {
+              scrollToSection(index + 1);
+              toggleIndicatorButton(index + 1);
+            }}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
       <h1 style={{ margin: "0", padding: "20px", color: "#e3e3e3" }}>
         Array Data Practices
       </h1>
-      <div className="data-board">
-        <AddValue />
-        <RemoveLastValue />
-        <RemoveFirstValue />
-        <InsertValueAtFirst />
-        <FindValue />
-        <SortArray />
-        <ReverseArray />
-        <FilterOutEvenValubes />
-        <TransformValues />
-        <SumAllValues />
+      {/* First Accordion */}
+      <div
+        onClick={() => toggleAccordion(1)}
+        className={`accordion ${openAccordion === 1 ? "open" : ""}`}
+      >
+        Basic Array Data Manipulation
+      </div>
+      <div
+        className={`data-board-container ${openAccordion === 1 ? "open" : ""}`}
+      >
+        <div className="data-board">
+          <div id="section1">
+            <AddValue />
+          </div>
+          <div id="section2">
+            <RemoveLastValue />
+          </div>
+          <div id="section3">
+            <RemoveFirstValue />
+          </div>
+          <div id="section4">
+            <InsertValueAtFirst />
+          </div>
+          <div id="section5">
+            <FindValue />
+          </div>
+          <div id="section6">
+            <SortArray />
+          </div>
+          <div id="section7">
+            <ReverseArray />
+          </div>
+          <div id="section8">
+            <FilterOutEvenValues />
+          </div>
+          <div id="section9">
+            <TransformValues />
+          </div>
+          <div id="section10">
+            <SumAllValues />
+          </div>
+        </div>
+      </div>
+      {/* Second Accordion */}
+      <div
+        onClick={() => toggleAccordion(2)}
+        className={`accordion ${openAccordion === 2 ? "open" : ""}`}
+      >
+        Intermediate Data Manipulation
+      </div>
+      <div
+        className={`data-board-container ${openAccordion === 2 ? "open" : ""}`}
+      >
+        <div className="data-board">
+          <div id="section1">
+            <SumAllValues />
+          </div>
+        </div>
+      </div>
+      {/* Third Accordion */}
+      <div
+        onClick={() => toggleAccordion(3)}
+        className={`accordion ${openAccordion === 3 ? "open" : ""}`}
+      >
+        Complex Array Data Manipulation
+      </div>
+      <div
+        className={`data-board-container ${openAccordion === 3 ? "open" : ""}`}
+      >
+        <div className="data-board">
+          <div id="section1">
+            <SumAllValues />
+          </div>
+        </div>
       </div>
     </div>
   );
